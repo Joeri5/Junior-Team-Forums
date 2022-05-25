172796d3a69afc2b21985ef0f928879dc12f33a0
Joeri <joerischenk@outlook.com>
2022-05-25 22:37:04 +0200

    almost finished the navbar (and responsive hamburger menu)

--- junior-team-client/components/Navbar.tsx
+++ junior-team-client/components/Navbar.tsx
@@ -4,13 +4,13 @@ import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import { Logo } from "../assets";
 
-type NavbarLink = {
+type NavbarLinkProps = {
   children: React.ReactNode;
   to: string;
   ignoreColor?: boolean;
 };
 
-function NavbarLink({ children, to, ignoreColor }: NavbarLink) {
+function NavbarLink({ children, to, ignoreColor }: NavbarLinkProps) {
   const router = useRouter();
 
   return ignoreColor ? (
@@ -21,15 +21,29 @@ function NavbarLink({ children, to, ignoreColor }: NavbarLink) {
       {children}
     </button>
   ) : router.asPath === to ? (
-    <button className="flex items-center gap-x-3 text-white bg-black text-xl px-10 py-2 rounded-xl">
+    <button className="flex border-[#fb561d] text-xl border-b-2 w-20 mx-10 md:mx-5 transitition-border ease-in-out duration-300">
       {children}
     </button>
   ) : (
     <button
       onClick={() => router.push(to)}
-      className="flex items-center gap-x-3 text-xl px-10 py-2 rounded-xl filter-img-child"
+      className="flex text-xl w-20 mx-10 md:mx-5 transitition-border ease-in-out duration-300"
     >
       {children}
+    </button>
+  );
+}
+
+function NavbarLogo(props: Props) {
+  const router = useRouter();
+
+  return router.asPath === "/" ? (
+    <button onClick={() => router.push("/")} className="text-2xl">
+      Junior <span className="text-[#fb561d]">Team</span>
+    </button>
+  ) : (
+    <button onClick={() => router.push("/")} className="text-2xl">
+      <span className="text-[#fb561d]">Junior</span> Team
     </button>
   );
 }
@@ -41,6 +55,7 @@ const Navbar = (props: Props) => {
   //   const [user, loading] = useAuthState(auth);
 
   const [isOpen, setIsOpen] = React.useState(false);
+  console.log(isOpen);
 
   useEffect(() => {
     if (isOpen) {
@@ -54,29 +69,59 @@ const Navbar = (props: Props) => {
     <>
       {!isOpen && (
         <>
-          <nav className="bg-gray-400">
-            <div>
-              <div className="px-5 py-5 flex items-center">
-              <h1 className="text-2xl">
-                Junior <span className="text-[#fb561d]">Team</span>
-              </h1>
-            </div>
-            <div>
-
-            </div>
+          <nav className="border-b shadow-sm">
+            <div className="flex justify-between w-screen px-5 md:px-10 py-5 items-center">
+              <div className="transition-all duration-300 ease-in-out">
+                <NavbarLogo />
+              </div>
+              <div className="hidden md:flex items-center">
+                <NavbarLink to="/topics">Topics</NavbarLink>
+                <NavbarLink to="/threads">Threads</NavbarLink>
+                <div className="h-10 ml-10">
+                  <button className="border-[#5865F2] border-2 hover:bg-[#5865F2] transition-colors ease-in-out duration-300 hover:text-white px-10 h-full rounded-md text-[#5865F2]">
+                    Login
+                  </button>
+                </div>
+              </div>
+              <div className="md:hidden">
+                <button onClick={() => setIsOpen(true)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-8 w-8 text-[#fb561d]"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                </button>
+              </div>
             </div>
           </nav>
         </>
       )}
 
       {isOpen && (
-        <>
-          <nav>
-            <div>
-              <h1>Junior Team</h1>
-            </div>
-          </nav>
-        </>
+        <motion.div
+          onClick={() => setIsOpen(!isOpen)}
+          animate={{ x: ["-100%", "0%"] }}
+          className="h-screen text-black md:hidden"
+        >
+          <div className="relative">
+            <button className="absolute top-10 right-10 text-3xl text-[#fb561d]">
+              ✕
+            </button>
+          </div>
+          <div className="py-10 flex flex-col gap-7">
+            <NavbarLink to="/topics">Topics</NavbarLink>
+            <NavbarLink to="/threads">Threads</NavbarLink>
+          </div>
+        </motion.div>
       )}
     </>
   );
---
+++ junior-team-client/pages/topics.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const topics = (props: Props) => {
+  return <div>topics</div>;
+};
+
+export default topics;
